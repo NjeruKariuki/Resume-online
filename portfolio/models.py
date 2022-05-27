36b21c1fee6b12a636b64cbd2fb8daa1b32d0972
Daniel Kariuki <freakoutbond2@gmail.com>
2022-05-27 18:39:38 +0300

    added ProjectImage model

--- portfolio/models.py
+++ portfolio/models.py
@@ -12,12 +12,20 @@ class Technology(models.Model):
     def __str__(self):
         return self.name
 
+class ProjectImage(models.Model):
+    name = models.CharField(max_length=50)
+    date_created = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='projects/', blank=True)
+
+    def __str__(self):
+        return self.name + ' image created' 
+
 class Project(models.Model):
     name = models.CharField(max_length=50)
-    technologies = models.ManyToManyField(Technology, blank=True, null=True)
+    technologies = models.ManyToManyField(Technology, blank=True)
     description = models.TextField(max_length=500, blank=True)
     git_link = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(upload_to='projects/', blank=True)
+    images = models.ManyToManyField(ProjectImage, blank=True)
 
     def __str__(self):
         return self.name
