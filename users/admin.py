a8d822bb1ed1f69622bd02d9f013b27d23738eb9
Daniel Kariuki <freakoutbond2@gmail.com>
2022-06-11 11:18:18 +0300

    registered skills model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile, ChiefImage
+from .models import Profile, ChiefImage, Skills
 
 admin.site.register(Profile)
 admin.site.register(ChiefImage)
+admin.site.register(Skills)
 
